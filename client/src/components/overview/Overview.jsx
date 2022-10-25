4d5125f38232be8bae7bfdf8cfebdae106823794
Michael Kim <dkim1017@gmail.com>
2022-10-24 20:27:51 -0700

    Update photos based on selected style

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -20,11 +20,14 @@ class Overview extends React.Component {
       ratings: {},
       starRating: [],
       photoURL: "https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
-      stylePhoto: []
+      stylePhoto: [],
+      selectedStyle: 123456
     }
     this.findProduct = this.findProduct.bind(this)
     this.findProductMeta = this.findProductMeta.bind(this)
     this.findProductStyles = this.findProductStyles.bind(this)
+    this.updateSelectedStyle = this.updateSelectedStyle.bind(this)
+    this.findDefaultStyle = this.findDefaultStyle.bind(this)
   }
 
   componentDidMount() {
@@ -97,27 +100,53 @@ class Overview extends React.Component {
         photos.push(tempObj)
       }
 
+      console.log(response.data.results)
       this.setState({
-        photoURL: response.data.results[0].photos[0].url,
+        styles: response.data.results,
         stylePhoto: photos,
-        selectedStyle: response.data.results[0].style_id
       })
+      this.findDefaultStyle(response.data.results)
     })
   }
 
+  findDefaultStyle(styles) {
+    for (var i = 0; i < styles.length; i++) {
+      if(styles[i]["default?"] === true) {
+        this.setState({
+          selectedStyle: styles[i].style_id
+        })
+        break;
+      }
+    }
+  }
 
+  updateSelectedStyle(e) {
+    e.preventDefault();
+    console.log("CLICK TARGET: ", e.target.alt)
+    this.setState({
+      selectedStyle: e.target.alt
+    })
+  }
+
+  // styles Carousel Object
+  // [
+  //   {
+  //     style_id: first thumbnail url
+  //   },
+  //   ...
+  // ]
 
   render() {
     return(
       <form>
         <div>
           <h1>Overview - Michael</h1>
-          <Picture url = {this.state.photoURL}/>
+          <Picture styles = {this.state.styles} selectedStyle = {this.state.selectedStyle}/>
           <BasicInfo category = {this.state.category} name = {this.state.name} price = {this.state.price}/>
           <StarReview totalStars = {this.state.totalStars} activeStars = {this.state.activeStars} ratings = {this.state.ratings}/>
           <a href="dummylink.com">Read all reviews</a>
           <Description slogan = {this.state.slogan} description = {this.state.description} />
-          <Styles stylePhoto = {this.state.stylePhoto} selectedStyle = {this.state.selectedStyle}/>
+          <Styles styles = {this.state.styles} stylePhoto = {this.state.stylePhoto} selectedStyle = {this.state.selectedStyle}  updateSelectedStyle = {(e) => this.updateSelectedStyle(e)}/>
         </div>
       </form>
     )
--- client/src/components/overview/Styles.jsx
+++ client/src/components/overview/Styles.jsx
@@ -4,10 +4,11 @@ class Styles extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      stylePhoto: [],
+      stylePhotos: [],
       selectedStylePhotos: []
     }
     this.firstThumbnail = this.firstThumbnail.bind(this)
+    this.firstThumbnailPhotos = this.firstThumbnailPhotos.bind(this)
   }
 
   // componentDiddMount() {
@@ -19,32 +20,58 @@ class Styles extends React.Component {
     if (this.props.stylePhoto !== prevProps.stylePhoto ||
       this.props.selectedStyle !== prevProps.selectedStyle) {
       this.firstThumbnail()
+      this.firstThumbnailPhotos()
     }
   }
 
+
   firstThumbnail() {
-    var photos = this.props.stylePhoto
-    this.setState({
-      stylePhoto: photos
-      // selectedStyle: Object.keys(this.props.stylePhoto[0])[0]
-    })
-    var selectedStyle = this.props.selectedStyle.toString()
-    for (var i = 0; i < photos.length; i++){
-      if(Object.keys(photos[i]).includes(selectedStyle)) {
-        var temp = photos[i][this.props.selectedStyle]
-        this.setState({selectedStylePhotos: [...temp]})
+    var styles = this.props.styles
+    console.log("STYLES: ", this.props.styles)
+    console.log("LENGTH: ", this.props.styles.length)
+    for (var i = 0; i < styles.length; i++) {
+      if(styles[i].style_id == this.props.selectedStyle) {
+        console.log("HITHITHIT: ", styles[i].style_id, this.props.selectedStyle)
+        this.setState( {
+          selectedStylePhotos: [...styles[i].photos]
+        })
         break;
       }
     }
+
+    // var photos = this.props.stylePhoto
+    // this.setState({
+    //   stylePhoto: photos
+    //   // selectedStyle: Object.keys(this.props.stylePhoto[0])[0]
+    // })
+    // var selectedStyle = this.props.selectedStyle.toString()
+    // for (var i = 0; i < photos.length; i++){
+    //   if(Object.keys(photos[i]).includes(selectedStyle)) {
+    //     var temp = photos[i][this.props.selectedStyle]
+    //     this.setState({selectedStylePhotos: [...temp]})
+    //     break;
+    //   }
+    // }
+  }
+
+  firstThumbnailPhotos() {
+    var styles = this.props.styles
+    var tempFirstThumbnails = []
+    for (var i = 0; i < styles.length; i++) {
+      tempFirstThumbnails.push(styles[i].photos[0]["thumbnail_url"])
+    }
+    this.setState( {
+      stylePhotos: tempFirstThumbnails
+    })
   }
 
   render() {
-    const { selectedStylePhotos } = this.state
+    const { selectedStylePhotos, stylePhotos } = this.state
     return (
       <div>
         <h1>Style Photos</h1>
-        {selectedStylePhotos ? (selectedStylePhotos.map((photo, index) => (
-          <img key={index} src={photo["thumbnail_url"]} alt="lalala" />
+        {stylePhotos ? (stylePhotos.map((photo, index) => (
+          <img key={index} src={photo} alt={this.props.styles[index].style_id} onClick = {this.props.updateSelectedStyle}/>
         )))
         : "Loading"
         }
