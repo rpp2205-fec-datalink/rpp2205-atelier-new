d2cc90fcfe46e6049ba9751c63050bc240e0ce1a
Michael Kim <dkim1017@gmail.com>
2022-11-05 02:23:27 -0700

    complete getting size and quantity dynamically

--- client/src/components/overview/SelectOption.jsx
+++ client/src/components/overview/SelectOption.jsx
@@ -10,13 +10,16 @@ class SelectOption extends React.Component {
     super(props);
     this.state = {
       sizeOptions: [
-        {label: "XS", value: "Extra Small"}
+        {label: "XS", value: "Extra Small"},
+        {label: "S", value: "Small"}
       ],
       quantityOptions: [
         {label: "1", value: "1"}
       ]
     }
     this.setValues = this.setValues.bind(this)
+    this.updateSkus = this.updateSkus.bind(this)
+    this.updateQuantity = this.updateQuantity.bind(this)
   }
 
   setValues(values){
@@ -27,15 +30,60 @@ class SelectOption extends React.Component {
     // update starRating state to starRating array
   }
 
+  componentDidUpdate(prevProps) {
+    if(this.props.selectedStyle !== prevProps.selectedStyle) {
+      this.updateSkus()
+    }
+  }
+
+  updateSkus() {
+    for (var i = 0; i < this.props.styles.length; i++) {
+      console.log("style id: ", this.props.styles[i].style_id)
+      if(this.props.styles[i].style_id == this.props.selectedStyle) {
+        console.log("111111111  ", this.props.styles[i].skus)
+        var tempSize = []
+        for (const key in this.props.styles[i].skus) {
+          var temp = this.props.styles[i].skus[key]["size"]
+          tempSize.push({
+            label: temp,
+            value: temp
+          })
+        }
+        console.log("Temp Size: ", tempSize)
+        this.setState({
+          sizeOption: [...tempSize],
+          currentSku: this.props.styles[i].skus
+        })
+      }
+    }
+  }
+
+  updateQuantity(e) {
+    console.log(e[0].value)
+    console.log("222222: ", this.state.currentSku)
+    for (const key in this.state.currentSku) {
+      if (this.state.currentSku[key]["size"] == e[0].value) {
+        const temp = this.state.currentSku[key]["quantity"]
+        const tempQuantity = []
+        for (var i = 0; i < temp; i++) {
+          tempQuantity.push({
+            label: i+1,
+            value: i+1
+          })
+        }
+        this.setState({
+          quantityOptions: [...tempQuantity]
+        })
+        break;
+      }
+    }
+  }
   render() {
     return(
       <div >
         <div className="dropdown-layout">
-          <Select className="dropdown-size" placeholder="Select Size" options={this.state.sizeOptions} onChange={() => null} />
-          <Select className="dropdown-quantity" placeholder="Select Quantity" options={this.state.quantityOptions} onChange={() => null} />
-{/*
-          <Dropdown className="dropdown-size" options={this.state.sizeOptions} value={this.state.defaultSizeOption} placeholder="Select Size" />
-          <Dropdown className="dropdown-quantity" options={this.state.quantityOptions} value={this.state.defaultQuantityOption} placeholder="Select Quantity" /> */}
+          <Select className="dropdown-size" placeholder="Select Size" options={this.state.sizeOption} onChange={this.updateQuantity} />
+          <Select className="dropdown-quantity" placeholder="Select Quantity" options={this.state.quantityOptions} onChange={() => console.log("change")} />
         </div>
         <div className="dropdown-layout">
           <p className="add-to-bag">Add To Bag +</p>
